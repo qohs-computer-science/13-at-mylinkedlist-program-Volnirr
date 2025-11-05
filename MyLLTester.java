79823abf0000faf544bcc158731d6ce533f1ef0f
Volnirr <101990817+Volnirr@users.noreply.github.com>
2025-11-05 17:46:32 +0000

    ready for check in 11/5

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,8 +1,20 @@
-public class MyLLTester.java
+public class MyLLTester
 {
   public static void main(String [] args)
   {
-    
+    MyLinkedList list = new MyLinkedList();
+    System.out.println("Created new list");
+
+    System.out.println("Size: " + list.size());
+    System.out.println("isEmpty: " + list.isEmpty());
+
+    System.out.println("toString: [" + list.toString() + "]");
+    System.out.println();
+
+    System.out.println("Adding values to empty list:");
+    list.add("First");
+    System.out.println("After add('First'): " + list.toString());
+    System.out.println();
 
   } // end main
   
